b6865b140db558edec9b91af1c512b3a733e6998
Ayisha EL-Refaie <ayisha.el-refaie@marquette.edu>
2024-05-02 14:53:20 -0500

    Made Vehicle Reservation Class

--- VehicleReservation.cs
+++ VehicleReservation.cs
@@ -5,10 +5,10 @@ public class VehicleReservation
     public  Reservation r {get; set;}
     public Vehicle v {get; set;}
 
-    public VehicleReservation(Reservation r, Vehicle v)
+    public VehicleReservation(Reservation r, Vehicle v) //Vehicle Reservation
     {
         this.r = r;
-        this.v = v;
+        this.v = v; 
     }
 
 }
