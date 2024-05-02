185c8dab68fdeeda3649ec62ec8797d7a79b648a
Ayisha EL-Refaie <ayisha.el-refaie@marquette.edu>
2024-05-02 14:51:03 -0500

    Made Vehicle Reservation Class

---
+++ VehicleReservation.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectINSY4051;
+
+public class VehicleReservation
+{
+    public  Reservation r {get; set;}
+    public Vehicle v {get; set;}
+
+    public VehicleReservation(Reservation r, Vehicle v)
+    {
+        this.r = r;
+        this.v = v;
+    }
+
+}
